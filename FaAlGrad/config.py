3d2a1b0950d3a801b252ef38acb1a3f161bb1b35
Nikita Malik <97782865+NikitaMalik2303@users.noreply.github.com>
2024-10-11 11:48:36 -0600

    Update config.py

--- FaAlGrad/config.py
+++ FaAlGrad/config.py
@@ -13,15 +13,15 @@ hidden_dim = [8, 16]
 output_dim = 1
 
 n_step = 1
-n_iter = 10 #number of innner loop iterations 
+n_iter = 10 
 
 inner_args = {
     'lr': 0.005     # Learning rate for inner-loop optimization
 }
 
 meta_args = {
-    'meta_train': True,     # Whether to train the model in meta-learning mode
-    'lr': 0.001,             # Learning rate for the meta-update step
-    'num_iter': 1           # num
+    'meta_train': True,     
+    'lr': 0.001,            
+    'num_iter': 1          
 }
 
